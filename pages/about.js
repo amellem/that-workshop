f4fa6c35fb76a8380385c097deb27bf90bd05a0f
Aerius <anthony.mellem85@gmail.com>
2019-08-05 10:15:38 -0500

    about.js modified

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 const About = () =>
     <ul>
         <li>
-            Josh is the smartest person on planet earth
+            Josh is the smartest person on planet earth, if the planet consisted of only him.
         </li>
     </ul>
 
